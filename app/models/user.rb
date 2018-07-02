ed76f2fe23d0e809fd5a1015e7af9297024b7bee
thanabodeekrathu <thanabodee.thkt@gmail.com>
2018-07-02 13:48:04 +0700

    user-organiz organiz-project

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  belongs_to :organization
+  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
